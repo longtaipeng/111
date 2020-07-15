b8daff0350675c98b0604aca3590aa9590c8fecc
longtaipeng <317742145@qq,com>
2020-07-15 15:19:54 +0800

    2020-07-15 参考得来

--- 1015 德才论.py
+++ 1015 德才论.py
@@ -8,3 +8,35 @@
 随后 N 行，每行给出一位考生的信息，包括：准考证号 德分 才分，其中准考证号为 8 位整数，德
 才分为区间 [0, 100] 内的整数。数字间以空格分隔。
 """
+
+
+def rank(x):
+    return x[3],int(x[1]),-int(x[0])
+a = [int(i) for i in input().split()]
+n = a[0]
+l = a[1]
+h = a[2]
+a = []; b = [];c = [];d = []
+for i in range(n):
+    x = input().split()
+    if int(x[1])<l or int(x[2])<l:
+        continue
+    x.append(int(x[1])+int(x[2]))
+    if int(x[1])>=h and int(x[2])>=h:
+        a.append(x)
+    elif int(x[1])>=h and int(x[2])<h:
+        b.append(x)
+    elif int(x[1])<h and int(x[2])<h and int(x[1])>=int(x[2]):
+        c.append(x)
+    else:
+        d.append(x)
+a = sorted(a,key=rank,reverse=True)
+b = sorted(b,key=rank,reverse=True)
+c = sorted(c,key=rank,reverse=True)
+d = sorted(d,key=rank,reverse=True)
+e = a + b + c + d
+print(len(e))
+for i in e:
+    print(' '.join(i[:-1]))
+
+
